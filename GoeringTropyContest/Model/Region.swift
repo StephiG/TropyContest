6584d8bf5d1bc6ed7ae0ed1259de5ae18abe9a19
John Goering <johngoering@me.com>
2022-10-07 17:56:46 +0200

    round picks struct prep

--- GoeringTropyContest/Model/Region.swift
+++ GoeringTropyContest/Model/Region.swift
@@ -11,4 +11,10 @@ struct Region: Codable, Equatable, Identifiable {
     var id = UUID()
     var teams: [Team]
     var name: String
+    
+    var isLeft: Bool {
+        get {
+            return name == "East" || name == "West"
+        }
+    }
 }
---
+++ GoeringTropyContest/Model/RoundPicks.swift
@@ -0,0 +1,15 @@
+//
+//  RoundPicks.swift
+//  GoeringTropyContest
+//
+//  Created by John Goering on 07.10.22.
+//
+
+import Foundation
+
+struct RoundPicks: Codable, Equatable, Identifiable {
+    var id = UUID()
+    var region:Region // which region
+    var round:Int // 0 = 64 round, 1 = 32 round, etc...
+    var picks:[Int:Team] = [:] // 0 = first (top) match, 1 = second (from top) match... 15 would be last match in round 64 for the region
+}
