2895e2ece8d5455a48ef86447136a2352637dda7
Stephi <stephigoering@me.com>
2022-11-14 19:19:24 +0100

    changed comment on rounds

--- GoeringTropyContest/Model/RoundPicks.swift
+++ GoeringTropyContest/Model/RoundPicks.swift
@@ -11,5 +11,5 @@ struct RoundPicks: Codable, Equatable, Identifiable {
     var id = UUID()
     var region:Region // which region
     var round:Int // 0 = 64 round, 1 = 32 round, etc...
-    var picks:[Int:Team] = [:] // 0 = first (top) match, 1 = second (from top) match... 15 would be last match in round 64 for the region
+    var picks:[Int:Team] = [:] // 0 = first (top) match, 1 = second (from top) match... 7 would be last match in round 64 for the region
 }
